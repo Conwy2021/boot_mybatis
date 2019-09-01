0ac500bfc15d0447f568ab72a1831dab561b6985
kangwei2019 <17628607521@163.com>
2019-09-02 03:24:27 +0800

    自动生成emp11

---
+++ src/main/java/com/example/demo/bean/Emp.java
@@ -0,0 +1,96 @@
+package com.example.demo.bean;
+
+import java.util.Date;
+
+public class Emp {
+    /**
+     * 员工编号
+     */
+    private Integer empno;
+
+    /**
+     * 员工名称
+     */
+    private String ename;
+
+    /**
+     * 岗位
+     */
+    private String job;
+
+    /**
+     * 上级编号
+     */
+    private Integer mgr;
+
+    /**
+     * 入职日期
+     */
+    private Date hiredate;
+
+    /**
+     * 工资
+     */
+    private Double sal;
+
+    /**
+     * 部门编号
+     */
+    private Integer deptno;
+
+    public Integer getEmpno() {
+        return empno;
+    }
+
+    public void setEmpno(Integer empno) {
+        this.empno = empno;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public Integer getMgr() {
+        return mgr;
+    }
+
+    public void setMgr(Integer mgr) {
+        this.mgr = mgr;
+    }
+
+    public Date getHiredate() {
+        return hiredate;
+    }
+
+    public void setHiredate(Date hiredate) {
+        this.hiredate = hiredate;
+    }
+
+    public Double getSal() {
+        return sal;
+    }
+
+    public void setSal(Double sal) {
+        this.sal = sal;
+    }
+
+    public Integer getDeptno() {
+        return deptno;
+    }
+
+    public void setDeptno(Integer deptno) {
+        this.deptno = deptno;
+    }
+}
---
+++ src/main/java/com/example/demo/mapper/EmpMapper.java
@@ -0,0 +1,17 @@
+package com.example.demo.mapper;
+
+import com.example.demo.bean.Emp;
+
+public interface EmpMapper {
+    int deleteByPrimaryKey(Integer empno);
+
+    int insert(Emp record);
+
+    int insertSelective(Emp record);
+
+    Emp selectByPrimaryKey(Integer empno);
+
+    int updateByPrimaryKeySelective(Emp record);
+
+    int updateByPrimaryKey(Emp record);
+}
