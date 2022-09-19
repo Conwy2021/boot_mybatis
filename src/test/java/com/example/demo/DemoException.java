8b51d4a5af3e4a08c5649ef10b189b31bfe2b88d
Conwy2021 <FQ06FQ06@gmail.com>
2022-09-20 00:25:40 +0800

    add nuls签名测试

--- src/test/java/com/example/demo/DemoException.java
+++ src/test/java/com/example/demo/DemoException.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
+import io.nuls.base.signture.SignatureUtil;
+import io.nuls.core.crypto.ECKey;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
 
@@ -68,4 +72,68 @@ public class DemoException {
 
 
     }
+    @Test
+    public  void test72() throws IOException {
+        String abc = stringToHexString("Angelillou");
+        System.out.println(abc);
+
+        byte[] data = {65, 110, 103, 101, 108, 105, 108, 108, 111, 117};// 原始数据
+        String privateKeyHex= "93d062c9790a289f928e21c9f3ac7b7ce6fe045d6aaf242d502cdcda6d9e0c84";
+        byte[] bytes = hexTobytes(privateKeyHex);
+        byte[] privateKey = bytes;// 私钥
+        ECKey ecKey = ECKey.fromPrivate(new BigInteger(1, privateKey));
+        byte[] signBytes = SignatureUtil.signDigest(data, ecKey).serialize();// 签名数据
+        String bytesToHex = bytesToHex(signBytes);
+        System.out.println(bytesToHex);// string 类型的签名数据 463044022100BBF57788C259EA317FA28E9035ACEF23273256D3E1F65CB48BA978308EFA5A5E021F723579724E2B6FB6CD2C3D85508A5A00B52EA9F5B3BD3C1176C9EFCACC1CD1
+        //公钥为0232bdaf6573319eba3b433ed88d6d4b0d06ea3ad9a504596fe967a2dbf95fc07c
+
+    }
+    @Test
+    public  void test90() {
+        String abc = stringToHexString("abc");
+        System.out.println(abc);
+        byte[] bytes = hexTobytes(abc);
+        System.out.println(Arrays.toString(bytes));
+
+    }
+
+
+
+    private static String stringToHexString(String s) {
+        String str = "";
+        for (int i = 0; i < s.length(); i++) {
+            int ch = s.charAt(i);
+            String c = Integer.toHexString(ch);
+            str = str + c;
+        }
+        return str;
+    }
+
+    public static byte[] hexTobytes(String hex) {
+        if (hex.length() < 1) {
+            return null;
+        } else {
+            byte[] result = new byte[hex.length() / 2];
+            int j = 0;
+            for(int i = 0; i < hex.length(); i+=2) {
+                result[j++] = (byte)Integer.parseInt(hex.substring(i,i+2), 16);
+            }
+            return result;
+        }
+    }
+
+    public static String bytesToHex(byte[] b) {
+        StringBuffer sb = new StringBuffer();
+        for(int i = 0; i < b.length; i++) {
+            String hex = Integer.toHexString(b[i] & 0xFF);
+            if(hex.length() < 2) {
+                hex = "0" + hex;
+            }
+            sb.append(hex.toUpperCase());
+        }
+
+        return sb.toString();
+    }
+
+
 }
